a62474a4502a84e0e87fd6301590fdb1288feeae
tarushivishwakarma <tarushivishwakarma22@gmail.com>
2026-06-22 22:41:34 +0530

    schemas added

---
+++ Backend/userModel.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+const studentSchema=new mongoose.Schema({
+    name:String,
+    email:{
+        type:String,
+        required:true
+    }
+    
+})
+module.exports=mongoose.model("Students",studentSchema) 
